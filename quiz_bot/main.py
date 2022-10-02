429f74ca22613d7be27dbc23e62a94bf6b380501
bobenkom <81431562+bobenkom@users.noreply.github.com>
2022-10-02 21:18:45 +0400

    new directory

--- quiz_bot/main.py
+++ quiz_bot/main.py
@@ -1,23 +1,31 @@
 import telebot
 import settings
+import time
+from datetime import datetime
 
 bot = telebot.TeleBot(settings.token)
 
+# Создаем список вопросов и ответов
 ls = ['кислород', 'водород', 'неон', 'титан', 'бром', 'хром', 'золото', 'серебро']
 questions = [
-            "Самый распространенный химический элемент на Земле?"
-            "Самый распространенный химический элемент во Вселенной?",
-            "Какой газ утверждает, что он – это не он?",
-            "Какой элемент является настоящим гигантом?",
-            "В названия каких химических элементов входит напиток морских пиратов?",
-            "Как называется химический элемент на картинке?",
-            ]
+    "Самый распространенный химический элемент на Земле?",
+    "Самый распространенный химический элемент во Вселенной?",
+    "Какой газ утверждает, что он – это не он?",
+    "Какой элемент является настоящим гигантом?",
+    "В названия каких химических элементов входит напиток морских пиратов?",
+    "Как называется химический элемент на картинке?",
+    "Как называется химический элемент на картинке?"
+]
 
+
+# Создаем функцию, которая запускается при неправильном ответе
 def game_over(message, err='Ошибка! '):
     bot.send_message(message.chat.id,
                      err + f'Викторина закончилась! Твой результат: {settings.users_dict[str(message.from_user.id)]}')
     settings.add_new_user(message.from_user.id, settings.users_dict[str(message.from_user.id)])
 
+
+# Создаем функцию, которая запускается при правильном ответе
 def ask_question(message, question_number):
     bot.send_message(message.chat.id, 'Правильно!')
     settings.users_dict[str(message.from_user.id)] += 1
@@ -29,11 +37,14 @@ def ask_question(message, question_number):
 def start(message):
     bot.send_message(message.chat.id, 'Я на связи. Начинаем викторину?'
                                       '\n/Start_quiz - Начать викторину'
-                                      '\n/Rating - Узнать свой рейтинг')
+                                      '\n/Rating - Узнать свой рейтинг'
+                                      '\n/Time- Узнать время за которое прошел викторину')
+
 
 @bot.message_handler(commands=['Start_quiz'])
 def quiz(message):
     settings.users_dict[str(message.from_user.id)] = 0
+    settings.time_players_dict[str(message.from_user.id)] = time.time()
     bot.send_message(message.chat.id, questions[0])
 
     @bot.message_handler(content_types=['text'])
@@ -68,7 +79,7 @@ def quiz(message):
     def get_answer_5(message):
         if message.text.lower() == ls[4] or message.text.lower() == ls[5]:
             ask_question(message, 5)
-            # bot.send_photo(message.chat.id, open('desktop/photo1.jpg', 'rb'))
+            bot.send_photo(message.chat.id, open('desktop/photo1.jpg', 'rb'))
             bot.register_next_step_handler(message, get_answer_6)
         else:
             game_over(message)
@@ -76,7 +87,7 @@ def quiz(message):
     def get_answer_6(message):
         if message.text.lower() == ls[6]:
             ask_question(message, 6)
-            # bot.send_photo(message.chat.id, open('desktop/photo2.jpg', 'rb'))
+            bot.send_photo(message.chat.id, open('desktop/photo2.jpg', 'rb'))
             bot.register_next_step_handler(message, get_answer_7)
         else:
             game_over(message)
@@ -85,13 +96,26 @@ def quiz(message):
         if message.text.lower() == ls[7]:
             ask_question(message, None)
             game_over(message, '')
+        quiz_time = time.time() - settings.time_players_dict[str(message.from_user.id)]
+        settings.get_time(message.from_user.id, quiz_time)
 
-@bot.message_handler(commands=['Rating'])
-def rate(message):
-    if str(message.from_user.id) not in settings.users_dict:
-        bot.send_message(message.chat.id, 'Ты еще не прошел викторину!'
-                                          '\n/Start_quiz - Начать викторину')
-    else:
-        bot.send_message(message.chat.id, f'Твое место в рейтинге викторины: {settings.get_rating(message.from_user.id)}')
+        # Создаем функцию, которая считает время, за которое мы прошли викторину
+        @bot.message_handler(commands=['Time'])
+        def time(message):
+            if str(message.from_user.id) not in settings.time_players_dict:
+                bot.send_message(message.chat.id, 'Ты еще не прошел викторину!'
+                                                  '\n/Start_quiz - Начать викторину')
+            else:
+                your_time = datetime.utcfromtimestamp(settings.time_players_dict[str(message.from_user.id)]).strftime('%M:%S')
+                bot.send_message(message.chat.id,f'Время, за которое ты прошел квиз: {your_time}')
 
-bot.polling(none_stop=True, interval=1)
+        @bot.message_handler(commands=['Rating'])
+        def rate(message):
+            if str(message.from_user.id) not in settings.users_dict:
+                bot.send_message(message.chat.id, 'Ты еще не прошел викторину!'
+                                                  '\n/Start_quiz - Начать викторину')
+            else:
+                bot.send_message(message.chat.id,
+                                 f'Твое место в рейтинге викторины: {settings.get_rating(message.from_user.id)}')
+
+        bot.polling(none_stop=True, interval=1)
